1d6e9471d673a71ab19814a103d3c7e3b5504647
Felix Le <felixle96@gmail.com>
2020-08-02 14:42:29 -0700

    Create Tower controller and data scripts

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower
+{
+    public Transform currentTarget;
+    public List<Transform> nearbyTargets;
+
+    private TowerSchematic towerSchematic;
+
+    public Tower(TowerSchematic _towerSchematic)
+    {
+        towerSchematic = _towerSchematic;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTargetEnterRange(Transform target)
+    {
+
+    }
+
+    public void OnTargetExitRange(Transform target)
+    {
+
+    }
+}
---
+++ Assets/Scripts/TowerSchematic.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TowerSchematic : ScriptableObject
+{
+    public string towerName;
+    public string towerDescription;
+    public int towerCost;
+    public int towerDamage;
+    public float towerRange;
+    public float towerFireRate;
+    public GameObject towerPrefab;
+
+    public abstract void ShootTarget(Transform target);
+
+    public virtual void BuildTower()
+    {
+        Instantiate(towerPrefab);
+    }
+
+    public virtual void BuildTower(Transform parent)
+    {
+        Instantiate(towerPrefab, parent);
+    }
+
+    public virtual void BuildTower(Vector3 position, Quaternion rotation)
+    {
+        Instantiate(towerPrefab, position, rotation);
+    }
+
+    public virtual void BuildTower(Vector3 position, Quaternion rotation, Transform parent)
+    {
+        Instantiate(towerPrefab, position, rotation, parent);
+    }
+
+    public virtual bool IsTargetInRange(Transform tower, Transform target)
+    {
+        // TODO
+        return true;
+    }
+
+    public Transform GetClosestInRangeTarget(Transform tower, List<Transform> nearbyTargets)
+    {
+        // TODO
+        return tower;
+    }
+
+    public int TowerCost(int amount)
+    {
+        return towerCost * amount;
+    }
+}
