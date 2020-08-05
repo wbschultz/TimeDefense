eea61ac26ae234de0bd8af0478473dbb9e433df6
Felix Le <felixle96@gmail.com>
2020-08-05 01:52:11 -0700

    Add Mouse enter/exit detection script for use with hovering on upgrade menu

---
+++ Assets/Scripts/DetectUIEvents.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public class BoolUnityEvent : UnityEvent<bool> { }
+
+public class DetectUIEvents : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public BoolUnityEvent onEnterUpgradeMenu;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        UnityEngine.Debug.Log("OnPointerEnter upgrade Menu");
+        onEnterUpgradeMenu.Invoke(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        UnityEngine.Debug.Log("OnPointerExit upgrade Menu");
+        onEnterUpgradeMenu.Invoke(false);
+    }
+}
