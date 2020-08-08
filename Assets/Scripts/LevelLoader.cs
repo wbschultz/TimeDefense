480bf499d496f86bcf1f01e45c8657d7233fec63
wbschultz <59841909+wbschultz@users.noreply.github.com>
2020-08-07 23:13:46 -0700

    Add and set up game-over scene

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelLoader : SingletonBase<LevelLoader>
+public class LevelLoader : MonoBehaviour
 {
     /// <summary>
     /// Load level by build index
