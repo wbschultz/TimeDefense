20173363831d2182079e4551a94d57ef16ee123c
wbschultz <59841909+wbschultz@users.noreply.github.com>
2020-08-07 21:46:23 -0700

    Add level loader singleton, hook up main menu

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : SingletonBase<LevelLoader>
+{
+    /// <summary>
+    /// Load level by build index
+    /// </summary>
+    /// <param name="index">build index of scene to load</param>
+    public void LoadLevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    /// <summary>
+    /// Load scene by name
+    /// </summary>
+    /// <param name="scene">name of scene to load</param>
+    public void LoadLevel(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    /// <summary>
+    /// Load level in build index order
+    /// </summary>
+    public void LoadNext()
+    {
+        LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
