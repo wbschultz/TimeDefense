45ed3976b276a4a483fff858650f8695ca8f821f
wbschultz <59841909+wbschultz@users.noreply.github.com>
2020-08-12 11:08:23 -0700

    Add layered sound SO and tweaked music manager

---
+++ Assets/Scripts/LayeredSound.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Layered Sound Clip", menuName = "Types/Layered Sound Clip")]
+public class LayeredSound : Sound
+{
+    [Header("Layer Info")]
+    public int currentLayerIndex = 0;
+    public List<Sound> layers = new List<Sound>();
+
+    public void NextLayer()
+    {
+        if (currentLayerIndex != layers.Count-1)
+        {
+            // increment track number
+            currentLayerIndex = Mathf.Min(layers.Count-1, currentLayerIndex + 1);
+
+            UpdateLayer();
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void ResetLayers()
+    {
+        currentLayerIndex = 0;
+
+        UpdateLayer();
+        
+    }
+
+    /// <summary>
+    /// update clip settings based on current layer
+    /// </summary>
+    private void UpdateLayer()
+    {
+        clip = layers[currentLayerIndex].clip;
+        volume = layers[currentLayerIndex].volume;
+        pitch = layers[currentLayerIndex].pitch;
+        loop = layers[currentLayerIndex].loop;
+    }
+}
--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,25 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
-/// <summary>
-/// Music manager: 
-/// Start music by calling PlayMusic()
-/// Stop Music by calling StopMusic()
-/// 
-/// To play an effect, call PlayEffect(AudioClip clip), where clip is the sound file you want to play
-/// 
-/// Adjust the volume by calling SetVolume(float vol), with vol being a float between 0 and 1.
-/// </summary>
+
+
 public class MusicManager : SingletonBase<MusicManager>
 {
     public List<Sound> mainTracks = new List<Sound>(); // list of sound clip Scriptable Objs for main theme
     public Sound title; // sound clip for title theme
     public float globalVol = 0.2f;
-    public AudioSource layer1, layer2, other;
+    public AudioSource layer1, layer2;
 
-    private int layerNumber;
     private ChanneledSource layerSource;
+    private Sound currSound;
 
+    #region ChanneledSource
     /// <summary>
     /// A container for 2 audiosource channels that allows switching clips without skipping
     /// </summary>
@@ -29,6 +23,11 @@ public class MusicManager : SingletonBase<MusicManager>
         private AudioSource channel2;
         private bool isMainChannel = true;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="m_channel1">AudioSource used for first channel</param>
+        /// <param name="m_channel2">AudioSource used for second channel</param>
         public ChanneledSource(AudioSource m_channel1, AudioSource m_channel2)
         {
             channel1 = m_channel1;
@@ -37,6 +36,10 @@ public class MusicManager : SingletonBase<MusicManager>
             channel2.volume = 0;
         }
 
+        /// <summary>
+        /// Get currently playing channel
+        /// </summary>
+        /// <returns>current channel's audio source</returns>
         public AudioSource GetChannel()
         {
             return isMainChannel ? channel1 : channel2;
@@ -52,30 +55,38 @@ public class MusicManager : SingletonBase<MusicManager>
             AudioSource nextChannel = isMainChannel ? channel2 : channel1;
             AudioSource currChannel = isMainChannel ? channel1 : channel2;
 
+            // set the clip information(except volume)
             nextChannel.clip = s.clip;
-            nextChannel.volume = s.volume;
             nextChannel.pitch = s.pitch;
             nextChannel.loop = s.loop;
 
+            // start playing new clip
+            nextChannel.Play();
+
+            // if they need to transition at the same time (layered tracks), set that
             if (sameTime)
             {
                 nextChannel.time = currChannel.time;
             }
-            nextChannel.Play();
+
+            // change volumes accordingly, and stop the previous clip
+            nextChannel.volume = s.volume;
             currChannel.volume = 0;
             currChannel.Stop();
             isMainChannel = !isMainChannel;
         }
     }
+    #endregion
 
-    private void Awake()
+    public override void Awake()
     {
+        base.Awake();
+        // setup 2 track source
         layerSource = new ChanneledSource(layer1, layer2);
     }
 
     private void Start()
     {
-        layerNumber = 0;
         SetGlobalVol(globalVol);
     }
 
@@ -84,19 +95,38 @@ public class MusicManager : SingletonBase<MusicManager>
         
     }
 
-    public void TransitionMusic()
+    /// <summary>
+    /// Add layer to currently playing track, iff it's a layered sound
+    /// </summary>
+    public void AddLayerToCurrent()
     {
-        if(layerNumber != 4)
+        // check if the current track is a layered track
+        LayeredSound currLayeredSound = currSound as LayeredSound;
+        if (currLayeredSound != null)
         {
-            // increment track number
-            layerNumber = Mathf.Min(4, layerNumber + 1);
+            // add layer and transition track
+            currLayeredSound.NextLayer();
+            layerSource.TransitionChannel(currLayeredSound, true);
+        }
+    }
 
-            // transition layer via channeled source
-            layerSource.TransitionChannel(mainTracks[layerNumber], true);
+    /// <summary>
+    /// Add layer to layered sound in sound list (doesn't have to be currently playing)
+    /// </summary>
+    /// <param name="index">index of layered sound in list</param>
+    public void AddLayer(int index)
+    {
+        // check if the current track is a layered track
+        LayeredSound layeredSound = mainTracks[index] as LayeredSound;
+        if (layeredSound != null)
+        {
+            // add layer and transition track
+            layeredSound.NextLayer();
         }
     }
 
 
+    // TODO change these to not be specific functions/integrate these with level loader
 
     /// <summary>
     /// play title theme
@@ -112,8 +142,8 @@ public class MusicManager : SingletonBase<MusicManager>
     /// </summary>
     public void PlayMain()
     {
+        // TODO change this to work off of table maybe?
         StopMusic();
-        layerNumber = 0;
         layerSource.TransitionChannel(mainTracks[0], false);
     }
 
@@ -123,11 +153,8 @@ public class MusicManager : SingletonBase<MusicManager>
     /// <param name="s">sound clip scriptable obj</param>
     public void PlayOther(Sound s)
     {
-        other.clip = s.clip;
-        other.volume = s.volume;
-        other.pitch = s.pitch;
-        other.loop = s.loop;
-        other.Play();
+        StopMusic();
+        layerSource.TransitionChannel(s, false);
     }
 
     /// <summary>
@@ -136,7 +163,6 @@ public class MusicManager : SingletonBase<MusicManager>
     public void StopMusic()
     {
         layerSource.GetChannel().Stop();
-        other.Stop();
     }
 
     /// <summary>
