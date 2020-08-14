1bea96be6cb7b7a35e08363afb035ce2eae43261
wbschultz <59841909+wbschultz@users.noreply.github.com>
2020-08-13 21:34:46 -0700

    Previous commit missed the camera follow script. Added in with this

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector2 velocity;
+
+    [SerializeField] float smoothTime = 0.3f;
+
+    private GameObject player;
+
+    private void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // smoothly update the camera position based on player position
+    private void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTime);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTime);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
+
